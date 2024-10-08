adc4f9a5570d8898009a592f3dabf4d039b0e8a4
salmanshahid5 <salmanshahid0333@gmail.com>
2024-10-08 15:52:55 +0500

    sections styling

--- src/components/Section.js
+++ src/components/Section.js
@@ -29,4 +29,64 @@ function Section(props) {
 
 export default Section;
 
+const Wrap = styled.div`
+  z-index:10;
+  width: 100vw;
+  height: 100vh;
+  background-image: url(${(props) => `/images/${props.bgImage}`});
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+  align-items: center;
+`;
 
+const ItemText = styled.div`
+  padding-top: 15vh;
+  text-align: center;
+`;
+
+const ButtonGroup = styled.div`
+  display: flex;
+  margin-bottom: 30px;
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+
+const LeftButton = styled.div`
+  background-color: rgba(23, 26, 32, 0.8);
+  height: 40px;
+  width: 256px;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 100px;
+  opacity: 0.85;
+  text-transform: uppercase;
+  font-size: 12px;
+  cursor: pointer;
+  margin: 8px;
+`;
+
+const RightButton = styled(LeftButton)`
+  background-color: white;
+  color: black;
+  opacity: 0.65;
+`;
+
+const DawnArrow = styled.img`
+  margin-top: 20px;
+  height: 40px;
+  overflow-x: hidden;
+  animation: animateDown infinite 1.5s;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
